c62550c414c4885e1a6dfc3d93f2a4538b66ae4b
joeybloggs <Dean.Karn@metricaid.com>
2015-07-10 11:31:53 -0400

    Investigating difference speed architectures
    investigation if goroutines in a consumer producer pattern vs passing a map
    Spoiler: the map wins out big time!

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -2,11 +2,11 @@ package validator
 
 import "testing"
 
-func BenchmarkValidateField(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		validate.Field("1", "len=1")
-	}
-}
+// func BenchmarkValidateField(b *testing.B) {
+// 	for n := 0; n < b.N; n++ {
+// 		validate.Field("1", "len=1")
+// 	}
+// }
 
 func BenchmarkValidateStructSimple(b *testing.B) {
 
@@ -42,122 +42,122 @@ func BenchmarkTemplateParallelSimple(b *testing.B) {
 	})
 }
 
-func BenchmarkValidateStructLarge(b *testing.B) {
+// func BenchmarkValidateStructLarge(b *testing.B) {
 
-	tFail := &TestString{
-		Required:  "",
-		Len:       "",
-		Min:       "",
-		Max:       "12345678901",
-		MinMax:    "",
-		Lt:        "0123456789",
-		Lte:       "01234567890",
-		Gt:        "1",
-		Gte:       "1",
-		OmitEmpty: "12345678901",
-		Sub: &SubTest{
-			Test: "",
-		},
-		Anonymous: struct {
-			A string `validate:"required"`
-		}{
-			A: "",
-		},
-		Iface: &Impl{
-			F: "12",
-		},
-	}
+// 	tFail := &TestString{
+// 		Required:  "",
+// 		Len:       "",
+// 		Min:       "",
+// 		Max:       "12345678901",
+// 		MinMax:    "",
+// 		Lt:        "0123456789",
+// 		Lte:       "01234567890",
+// 		Gt:        "1",
+// 		Gte:       "1",
+// 		OmitEmpty: "12345678901",
+// 		Sub: &SubTest{
+// 			Test: "",
+// 		},
+// 		Anonymous: struct {
+// 			A string `validate:"required"`
+// 		}{
+// 			A: "",
+// 		},
+// 		Iface: &Impl{
+// 			F: "12",
+// 		},
+// 	}
 
-	tSuccess := &TestString{
-		Required:  "Required",
-		Len:       "length==10",
-		Min:       "min=1",
-		Max:       "1234567890",
-		MinMax:    "12345",
-		Lt:        "012345678",
-		Lte:       "0123456789",
-		Gt:        "01234567890",
-		Gte:       "0123456789",
-		OmitEmpty: "",
-		Sub: &SubTest{
-			Test: "1",
-		},
-		SubIgnore: &SubTest{
-			Test: "",
-		},
-		Anonymous: struct {
-			A string `validate:"required"`
-		}{
-			A: "1",
-		},
-		Iface: &Impl{
-			F: "123",
-		},
-	}
+// 	tSuccess := &TestString{
+// 		Required:  "Required",
+// 		Len:       "length==10",
+// 		Min:       "min=1",
+// 		Max:       "1234567890",
+// 		MinMax:    "12345",
+// 		Lt:        "012345678",
+// 		Lte:       "0123456789",
+// 		Gt:        "01234567890",
+// 		Gte:       "0123456789",
+// 		OmitEmpty: "",
+// 		Sub: &SubTest{
+// 			Test: "1",
+// 		},
+// 		SubIgnore: &SubTest{
+// 			Test: "",
+// 		},
+// 		Anonymous: struct {
+// 			A string `validate:"required"`
+// 		}{
+// 			A: "1",
+// 		},
+// 		Iface: &Impl{
+// 			F: "123",
+// 		},
+// 	}
 
-	for n := 0; n < b.N; n++ {
-		validate.Struct(tSuccess)
-		validate.Struct(tFail)
-	}
-}
+// 	for n := 0; n < b.N; n++ {
+// 		validate.Struct(tSuccess)
+// 		validate.Struct(tFail)
+// 	}
+// }
 
-func BenchmarkTemplateParallelLarge(b *testing.B) {
+// func BenchmarkTemplateParallelLarge(b *testing.B) {
 
-	tFail := &TestString{
-		Required:  "",
-		Len:       "",
-		Min:       "",
-		Max:       "12345678901",
-		MinMax:    "",
-		Lt:        "0123456789",
-		Lte:       "01234567890",
-		Gt:        "1",
-		Gte:       "1",
-		OmitEmpty: "12345678901",
-		Sub: &SubTest{
-			Test: "",
-		},
-		Anonymous: struct {
-			A string `validate:"required"`
-		}{
-			A: "",
-		},
-		Iface: &Impl{
-			F: "12",
-		},
-	}
+// 	tFail := &TestString{
+// 		Required:  "",
+// 		Len:       "",
+// 		Min:       "",
+// 		Max:       "12345678901",
+// 		MinMax:    "",
+// 		Lt:        "0123456789",
+// 		Lte:       "01234567890",
+// 		Gt:        "1",
+// 		Gte:       "1",
+// 		OmitEmpty: "12345678901",
+// 		Sub: &SubTest{
+// 			Test: "",
+// 		},
+// 		Anonymous: struct {
+// 			A string `validate:"required"`
+// 		}{
+// 			A: "",
+// 		},
+// 		Iface: &Impl{
+// 			F: "12",
+// 		},
+// 	}
 
-	tSuccess := &TestString{
-		Required:  "Required",
-		Len:       "length==10",
-		Min:       "min=1",
-		Max:       "1234567890",
-		MinMax:    "12345",
-		Lt:        "012345678",
-		Lte:       "0123456789",
-		Gt:        "01234567890",
-		Gte:       "0123456789",
-		OmitEmpty: "",
-		Sub: &SubTest{
-			Test: "1",
-		},
-		SubIgnore: &SubTest{
-			Test: "",
-		},
-		Anonymous: struct {
-			A string `validate:"required"`
-		}{
-			A: "1",
-		},
-		Iface: &Impl{
-			F: "123",
-		},
-	}
+// 	tSuccess := &TestString{
+// 		Required:  "Required",
+// 		Len:       "length==10",
+// 		Min:       "min=1",
+// 		Max:       "1234567890",
+// 		MinMax:    "12345",
+// 		Lt:        "012345678",
+// 		Lte:       "0123456789",
+// 		Gt:        "01234567890",
+// 		Gte:       "0123456789",
+// 		OmitEmpty: "",
+// 		Sub: &SubTest{
+// 			Test: "1",
+// 		},
+// 		SubIgnore: &SubTest{
+// 			Test: "",
+// 		},
+// 		Anonymous: struct {
+// 			A string `validate:"required"`
+// 		}{
+// 			A: "1",
+// 		},
+// 		Iface: &Impl{
+// 			F: "123",
+// 		},
+// 	}
 
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			validate.Struct(tSuccess)
-			validate.Struct(tFail)
-		}
-	})
-}
+// 	b.RunParallel(func(pb *testing.PB) {
+// 		for pb.Next() {
+// 			validate.Struct(tSuccess)
+// 			validate.Struct(tFail)
+// 		}
+// 	})
+// }
--- examples_test.go
+++ examples_test.go
@@ -1,95 +1,89 @@
 package validator_test
 
-import (
-	"fmt"
+// func ExampleValidate_new() {
+// 	validator.New("validate", validator.BakedInValidators)
+// }
 
-	"../validator"
-)
+// func ExampleValidate_addFunction() {
+// 	// This should be stored somewhere globally
+// 	var validate *validator.Validate
 
-func ExampleValidate_new() {
-	validator.New("validate", validator.BakedInValidators)
-}
+// 	validate = validator.New("validate", validator.BakedInValidators)
 
-func ExampleValidate_addFunction() {
-	// This should be stored somewhere globally
-	var validate *validator.Validate
+// 	fn := func(top interface{}, current interface{}, field interface{}, param string) bool {
+// 		return field.(string) == "hello"
+// 	}
 
-	validate = validator.New("validate", validator.BakedInValidators)
+// 	validate.AddFunction("valueishello", fn)
 
-	fn := func(top interface{}, current interface{}, field interface{}, param string) bool {
-		return field.(string) == "hello"
-	}
+// 	message := "hello"
+// 	err := validate.Field(message, "valueishello")
+// 	fmt.Println(err)
+// 	//Output:
+// 	//<nil>
+// }
 
-	validate.AddFunction("valueishello", fn)
+// func ExampleValidate_field() {
+// 	// This should be stored somewhere globally
+// 	var validate *validator.Validate
 
-	message := "hello"
-	err := validate.Field(message, "valueishello")
-	fmt.Println(err)
-	//Output:
-	//<nil>
-}
+// 	validate = validator.New("validate", validator.BakedInValidators)
 
-func ExampleValidate_field() {
-	// This should be stored somewhere globally
-	var validate *validator.Validate
+// 	i := 0
+// 	err := validate.Field(i, "gt=1,lte=10")
+// 	fmt.Println(err.Field)
+// 	fmt.Println(err.Tag)
+// 	fmt.Println(err.Kind) // NOTE: Kind and Type can be different i.e. time Kind=struct and Type=time.Time
+// 	fmt.Println(err.Type)
+// 	fmt.Println(err.Param)
+// 	fmt.Println(err.Value)
+// 	//Output:
+// 	//
+// 	//gt
+// 	//int
+// 	//int
+// 	//1
+// 	//0
+// }
 
-	validate = validator.New("validate", validator.BakedInValidators)
+// func ExampleValidate_struct() {
+// 	// This should be stored somewhere globally
+// 	var validate *validator.Validate
 
-	i := 0
-	err := validate.Field(i, "gt=1,lte=10")
-	fmt.Println(err.Field)
-	fmt.Println(err.Tag)
-	fmt.Println(err.Kind) // NOTE: Kind and Type can be different i.e. time Kind=struct and Type=time.Time
-	fmt.Println(err.Type)
-	fmt.Println(err.Param)
-	fmt.Println(err.Value)
-	//Output:
-	//
-	//gt
-	//int
-	//int
-	//1
-	//0
-}
+// 	validate = validator.New("validate", validator.BakedInValidators)
 
-func ExampleValidate_struct() {
-	// This should be stored somewhere globally
-	var validate *validator.Validate
+// 	type ContactInformation struct {
+// 		Phone  string `validate:"required"`
+// 		Street string `validate:"required"`
+// 		City   string `validate:"required"`
+// 	}
 
-	validate = validator.New("validate", validator.BakedInValidators)
+// 	type User struct {
+// 		Name               string `validate:"required,excludesall=!@#$%^&*()_+-=:;?/0x2C"` // 0x2C = comma (,)
+// 		Age                int8   `validate:"required,gt=0,lt=150"`
+// 		Email              string `validate:"email"`
+// 		ContactInformation []*ContactInformation
+// 	}
 
-	type ContactInformation struct {
-		Phone  string `validate:"required"`
-		Street string `validate:"required"`
-		City   string `validate:"required"`
-	}
+// 	contactInfo := &ContactInformation{
+// 		Street: "26 Here Blvd.",
+// 		City:   "Paradeso",
+// 	}
 
-	type User struct {
-		Name               string `validate:"required,excludesall=!@#$%^&*()_+-=:;?/0x2C"` // 0x2C = comma (,)
-		Age                int8   `validate:"required,gt=0,lt=150"`
-		Email              string `validate:"email"`
-		ContactInformation []*ContactInformation
-	}
+// 	user := &User{
+// 		Name:               "Joey Bloggs",
+// 		Age:                31,
+// 		Email:              "joeybloggs@gmail.com",
+// 		ContactInformation: []*ContactInformation{contactInfo},
+// 	}
 
-	contactInfo := &ContactInformation{
-		Street: "26 Here Blvd.",
-		City:   "Paradeso",
-	}
-
-	user := &User{
-		Name:               "Joey Bloggs",
-		Age:                31,
-		Email:              "joeybloggs@gmail.com",
-		ContactInformation: []*ContactInformation{contactInfo},
-	}
-
-	structError := validate.Struct(user)
-	for _, fieldError := range structError.Errors {
-		fmt.Println(fieldError.Field) // Phone
-		fmt.Println(fieldError.Tag)   // required
-		//... and so forth
-		//Output:
-		//Phone
-		//required
-	}
-}
+// 	structError := validate.Struct(user)
+// 	for _, fieldError := range structError.Errors {
+// 		fmt.Println(fieldError.Field) // Phone
+// 		fmt.Println(fieldError.Tag)   // required
+// 		//... and so forth
+// 		//Output:
+// 		//Phone
+// 		//required
+// 	}
+// }
