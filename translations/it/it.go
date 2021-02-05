bc8247ccae1f2a40260d47dac38f0700b1c01fad
mrkongo <20625823+mrkongo@users.noreply.github.com>
2021-02-05 02:53:53 +0100

    Update it.go

--- translations/it/it.go
+++ translations/it/it.go
@@ -30,6 +30,11 @@ func RegisterDefaultTranslations(v *validator.Validate, trans ut.Translator) (er
 			override:    false,
 		},
 		{
+			tag:         "required_without",
+			translation: "{0} è un campo obbligatorio",
+			override:    false,
+		},
+		{
 			tag: "len",
 			customRegisFunc: func(ut ut.Translator) (err error) {
 
