7dc7bf859dc52e064fd67139f85f141520ca0706
gosua <gosua@users.noreply.github.com>
2021-11-18 15:07:39 +0100

    Enhance example in function docu

--- validator_instance.go
+++ validator_instance.go
@@ -180,6 +180,7 @@ func (v *Validate) ValidateMap(data map[string]interface{}, rules map[string]int
 //
 //    validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
 //        name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+//        // skip if tag key says it should be ignored
 //        if name == "-" {
 //            return ""
 //        }
