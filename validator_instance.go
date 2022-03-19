3561a9ccdb1ada5e3bc191fac3706b3220ab7e63
leftjs <lefttjs@gmail.com>
2022-03-19 15:39:20 +0800

    Enhanced ValidationCtx method to support nested map in slice #915

--- validator_instance.go
+++ validator_instance.go
@@ -152,19 +152,35 @@ func (v *Validate) SetTagName(name string) {
 func (v Validate) ValidateMapCtx(ctx context.Context, data map[string]interface{}, rules map[string]interface{}) map[string]interface{} {
 	errs := make(map[string]interface{})
 	for field, rule := range rules {
-		if reflect.ValueOf(rule).Kind() == reflect.Map && reflect.ValueOf(data[field]).Kind() == reflect.Map {
-			err := v.ValidateMapCtx(ctx, data[field].(map[string]interface{}), rule.(map[string]interface{}))
-			if len(err) > 0 {
-				errs[field] = err
-			}
-		} else if reflect.ValueOf(rule).Kind() == reflect.Map {
-			errs[field] = errors.New("The field: '" + field + "' is not a map to dive")
-		} else {
+		if reflect.ValueOf(rule).Kind() != reflect.Map {
 			err := v.VarCtx(ctx, data[field], rule.(string))
 			if err != nil {
 				errs[field] = err
 			}
+			continue
 		}
+
+		if kind := reflect.ValueOf(data[field]).Kind(); kind == reflect.Map {
+			err := v.ValidateMapCtx(ctx, data[field].(map[string]interface{}), rule.(map[string]interface{}))
+			if len(err) > 0 {
+				errs[field] = err
+			}
+		} else if kind == reflect.Slice {
+			for _, item := range data[field].([]interface{}) {
+				if reflect.ValueOf(item).Kind() != reflect.Map {
+					errs[field] = errors.New("The field: '" + field + "' is not a []map to dive")
+					continue
+				}
+
+				err := v.ValidateMapCtx(ctx, item.(map[string]interface{}), rule.(map[string]interface{}))
+				if len(err) > 0 {
+					errs[field] = err
+				}
+			}
+		} else {
+			errs[field] = errors.New("The field: '" + field + "' is not a map to dive")
+		}
+
 	}
 	return errs
 }
