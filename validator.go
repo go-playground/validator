4c392526a088c3b063d7daecb5500c8e0ddd9b0c
Jacob <jacob.ryan2@capitalone.com>
2023-06-22 17:36:56 -0400

    Set current typeof to typeExcluded for checking in struct validation

--- validator.go
+++ validator.go
@@ -170,7 +170,7 @@ func (v *validate) traverseField(ctx context.Context, parent reflect.Value, curr
 
 				if ct.typeof == typeStructOnly {
 					goto CONTINUE
-				} else if ct.typeof == typeIsDefault {
+				} else if ct.typeof == typeIsDefault || ct.typeof == typeExcluded {
 					// set Field Level fields
 					v.slflParent = parent
 					v.flField = current
@@ -201,7 +201,9 @@ func (v *validate) traverseField(ctx context.Context, parent reflect.Value, curr
 								typ:            typ,
 							},
 						)
-						return
+						if ct.typeof == typeIsDefault {
+							return
+						}
 					}
 				}
 
