8dda4f620926f6586708199bf90c697787243b23
niciosity <66202275+niciosity@users.noreply.github.com>
2023-06-30 15:52:24 +0700

    Change behavior of hasValue to be able to tell if struct is zero

--- baked_in.go
+++ baked_in.go
@@ -1866,7 +1866,11 @@ func isDefault(fl FieldLevel) bool {
 // hasValue is the validation function for validating if the current field's value is not the default static value.
 func hasValue(fl FieldLevel) bool {
 	fmt.Println("in hasValue")
+
 	field := fl.Field()
+
+	fmt.Printf("field Kind %s", field.Kind())
+
 	switch field.Kind() {
 	case reflect.Slice, reflect.Map, reflect.Ptr, reflect.Interface, reflect.Chan, reflect.Func:
 		return !field.IsNil()
