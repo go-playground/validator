6b04ccca8920fd4383bdd2390e9c7165a85f5352
Violin <violin0622@outlook.com>
2024-01-07 16:59:15 +0800

    fix(translations/en): fix test case error

--- translations/en/en_test.go
+++ translations/en/en_test.go
@@ -124,6 +124,7 @@ func TestTranslations(t *testing.T) {
 		ISBN               string            `validate:"isbn"`
 		ISBN10             string            `validate:"isbn10"`
 		ISBN13             string            `validate:"isbn13"`
+		ISSN               string            `validate:"issn"`
 		UUID               string            `validate:"uuid"`
 		UUID3              string            `validate:"uuid3"`
 		UUID4              string            `validate:"uuid4"`
