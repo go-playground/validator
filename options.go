82b92900962c3b4f726f6cacb720b3c13656b1ea
Gustavo <gustavonicolaump@gmail.com>
2025-02-13 00:28:24 -0300

    feat: add description in withearlyexit option

--- options.go
+++ options.go
@@ -25,7 +25,10 @@ func WithPrivateFieldValidation() Option {
 	}
 }
 
-// WithEarlyExit
+// WithEarlyExit configures the validator to immediately stop validation as soon as the first error is encountered
+//
+// This feature could be an opt-in behavior, allowing to opt into "early exit" validation, without breaking current workflows
+// Early exit on the first failure would save time by avoiding unnecessary checks on the remaining fields
 func WithEarlyExit() Option {
 	return func(v *Validate) {
 		v.earlyExit = true
