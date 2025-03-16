d7f7722e3c7ed9ea6841a56ef1b110e56098f0eb
Benitah <elad@sayanchor.com>
2025-03-16 12:48:45 +0200

    fix: add another test case

--- validator_test.go
+++ validator_test.go
@@ -8737,6 +8737,10 @@ func TestEmail(t *testing.T) {
 	NotEqual(t, errs, nil)
 	AssertError(t, errs, "", "", "", "", "email")
 
+	s = "Foo Bar <foobar@example.com>"
+	errs = validate.Var(s, "email")
+	NotEqual(t, errs, nil)
+
 	s = ""
 	errs = validate.Var(s, "email")
 	NotEqual(t, errs, nil)
