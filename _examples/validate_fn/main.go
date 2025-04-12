7b6b95e0aa61e17f5daeb35f6bac5853760e12d9
Tiago Peczenyj <tpeczenyj@weborama.com>
2025-04-12 20:12:14 +0200

    improve example

--- _examples/validate_fn/main.go
+++ _examples/validate_fn/main.go
@@ -7,17 +7,15 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
+//go:generate enumer -type=Enum
 type Enum uint8
 
 const (
 	Zero Enum = iota
 	One
 	Two
+	Three
 )
-
-func (e Enum) NotZero() bool {
-	return e != Zero
-}
 
 func (e *Enum) Validate() error {
 	if e == nil {
@@ -29,13 +27,15 @@ func (e *Enum) Validate() error {
 
 type Struct struct {
 	Foo *Enum `validate:"validateFn"`         // uses Validate() error by default
-	Bar Enum  `validate:"validateFn=NotZero"` // uses NotZero() bool
+	Bar Enum  `validate:"validateFn=IsAEnum"` // uses IsAEnum() bool provided by enumer
 }
 
 func main() {
 	validate := validator.New()
 
 	var x Struct
+
+	x.Bar = Enum(64)
 
 	if err := validate.Struct(x); err != nil {
 		fmt.Printf("Expected Err(s):\n%+v\n", err)
