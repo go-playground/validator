afc74596cfc84ee2fc5853d94e0c0896d92c3fa3
Yan Kalcheevskiy <yan.kalchevskiy@gmail.com>
2025-05-01 01:00:07 +0400

    Remove TODO

--- util.go
+++ util.go
@@ -296,7 +296,7 @@ func fieldMatchesRegexByStringerValOrString(regexFn func() *regexp.Regexp, fl Fi
 	case reflect.String:
 		return regex.MatchString(fl.Field().String())
 	default:
-		// TODO: return regex.MatchString(fmt.Sprintf("%s", fl.Field())) // should be enoungh, add tests to check //nolint:godox
+		// return regex.MatchString(fmt.Sprintf("%s", fl.Field())) // should be enoungh, add tests to check separately
 		if stringer, ok := getValue(fl.Field()).(fmt.Stringer); ok {
 			return regex.MatchString(stringer.String())
 		} else {
