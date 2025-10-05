94e89f0942028ebfb17426b8d3c2005a46557621
Ligt Nguyen <nguyenduyquang06@gmail.com>
2025-10-05 09:26:43 +0700

    fix: should panic when define duplicate field param in `required_if` (#1468)
    ## Fixes Or Enhances
    fix #1466 
    
    **Make sure that you've checked the boxes below before you submit PR:**
    - [x] Tests exist or have been written that cover this particular
    change.
    
    @go-playground/validator-maintainers
    
    ---------
    
    Co-authored-by: nodivbyzero <nodivbyzero@gmail.com>

--- doc.go
+++ doc.go
@@ -264,6 +264,7 @@ The field under validation must be present and not empty only if all
 the other specified fields are equal to the value following the specified
 field. For strings ensures value is not "". For slices, maps, pointers,
 interfaces, channels and functions ensures the value is not nil. For structs ensures value is not the zero value.
+Using the same field name multiple times in the parameters will result in a panic at runtime.
 
 	Usage: required_if
 
