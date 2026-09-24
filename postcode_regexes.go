a2d274fc9c647e0e2c0fa03a6c9a73a08479f7d3
beduldul <alkaysan07@gmail.com>
2026-09-24 23:48:32 +0700

    fix: anchor GB and LI postcode patterns as a whole
    The GB and LI patterns in postcode_regexes.go put a top level `|` between
    `^` and `$`. In RE2 those are ordinary assertions that apply only to the
    branch they are written in, so `^a|b$` parses as `(^a)|(b$)` and the
    middle branch is left unanchored.
    
    As a result `postcode_iso3166_alpha2=GB` accepted any string ending in a
    valid UK postcode, or in a `BFPO nnnn` token, and
    `postcode_iso3166_alpha2=LI` accepted any string ending in 9490-9497.
    `junkEC1A 1BB`, `prefixBFPO 1234` and `9485junk` all validated.
    
    Wrapping each alternation in a non-capturing group makes the anchors cover
    the whole pattern, which is how the other 159 entries are already written.
    Tests for both countries are added to TestPostCodeByIso3166Alpha2.
    
    Co-Authored-By: DeepSeek <noreply@deepseek.com>

--- postcode_regexes.go
+++ postcode_regexes.go
@@ -6,7 +6,7 @@ import (
 )
 
 var postCodePatternDict = map[string]string{
-	"GB": `^GIR[ ]?0AA|((AB|AL|B|BA|BB|BD|BH|BL|BN|BR|BS|BT|CA|CB|CF|CH|CM|CO|CR|CT|CV|CW|DA|DD|DE|DG|DH|DL|DN|DT|DY|E|EC|EH|EN|EX|FK|FY|G|GL|GY|GU|HA|HD|HG|HP|HR|HS|HU|HX|IG|IM|IP|IV|JE|KA|KT|KW|KY|L|LA|LD|LE|LL|LN|LS|LU|M|ME|MK|ML|N|NE|NG|NN|NP|NR|NW|OL|OX|PA|PE|PH|PL|PO|PR|RG|RH|RM|S|SA|SE|SG|SK|SL|SM|SN|SO|SP|SR|SS|ST|SW|SY|TA|TD|TF|TN|TQ|TR|TS|TW|UB|W|WA|WC|WD|WF|WN|WR|WS|WV|YO|ZE)(\d[\dA-Z]?[ ]?\d[ABD-HJLN-UW-Z]{2}))|BFPO[ ]?\d{1,4}$`,
+	"GB": `^(?:GIR[ ]?0AA|((AB|AL|B|BA|BB|BD|BH|BL|BN|BR|BS|BT|CA|CB|CF|CH|CM|CO|CR|CT|CV|CW|DA|DD|DE|DG|DH|DL|DN|DT|DY|E|EC|EH|EN|EX|FK|FY|G|GL|GY|GU|HA|HD|HG|HP|HR|HS|HU|HX|IG|IM|IP|IV|JE|KA|KT|KW|KY|L|LA|LD|LE|LL|LN|LS|LU|M|ME|MK|ML|N|NE|NG|NN|NP|NR|NW|OL|OX|PA|PE|PH|PL|PO|PR|RG|RH|RM|S|SA|SE|SG|SK|SL|SM|SN|SO|SP|SR|SS|ST|SW|SY|TA|TD|TF|TN|TQ|TR|TS|TW|UB|W|WA|WC|WD|WF|WN|WR|WS|WV|YO|ZE)(\d[\dA-Z]?[ ]?\d[ABD-HJLN-UW-Z]{2}))|BFPO[ ]?\d{1,4})$`,
 	"JE": `^JE\d[\dA-Z]?[ ]?\d[ABD-HJLN-UW-Z]{2}$`,
 	"GG": `^GY\d[\dA-Z]?[ ]?\d[ABD-HJLN-UW-Z]{2}$`,
 	"IM": `^IM\d[\dA-Z]?[ ]?\d[ABD-HJLN-UW-Z]{2}$`,
@@ -78,7 +78,7 @@ var postCodePatternDict = map[string]string{
 	"LA": `^\d{5}$`,
 	"LV": `^\d{4}$`,
 	"LB": `^(\d{4}([ ]?\d{4})?)?$`,
-	"LI": `^(948[5-9])|(949[0-7])$`,
+	"LI": `^(?:(948[5-9])|(949[0-7]))$`,
 	"LT": `^\d{5}$`,
 	"LU": `^\d{4}$`,
 	"MK": `^\d{4}$`,
